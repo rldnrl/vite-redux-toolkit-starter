e29342df2d50d610911ac30c83faa0d42d07aa48
Park Kiwook <81848094+vueveloper@users.noreply.github.com>
2021-10-17 13:42:07 +0900

    feat: define type app thunk

--- src/store/hook.ts
+++ src/store/hook.ts
@@ -4,3 +4,9 @@ import type { RootState, AppDispatch } from ".";
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
